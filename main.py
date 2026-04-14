a9854ce73110d23b293da63aaec42cf301362be4
Anton -- Lev1nzz <xlev1n.xxxx@gmail.com>
2026-04-14 19:55:46 +0300

    add redirect endpoint

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ import string
 from uuid import uuid4
 from dotenv import load_dotenv
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 from sqlalchemy import Column, String
 
-
+# функция для работы с переменными окружения
 load_dotenv()
 
+# переменные окружения
 DATABASE_URL = os.getenv('DATABASE_URL')
 SHORT_URL_LEN = int(os.getenv('SHORT_URL_LEN', '50')) 
 BASE_URL_LEN = int(os.getenv('BASE_URL_LEN', '500'))
@@ -40,7 +42,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # список всех символов
 all_chars = list(string.ascii_letters + string.digits)
 
-
+# функция генерации короткой ссылки
 def generate_short_url(all_chars: list[str]):
     
     short_url_slug = ''
@@ -72,8 +74,12 @@ class CreateUrlSchema(BaseModel):
     url: str
 
 
+
+# ручка добавления в бд короткого и длинного урла
 @app.post('/short_url')
-def create_short_url(payload: CreateUrlSchema, db: Session = Depends(get_db)) -> UrlSchema:
+def create_short_url(
+    payload: CreateUrlSchema, db: Session = Depends(get_db)
+    ) -> UrlSchema:
 
         short_slug = generate_short_url(all_chars)
         
@@ -89,3 +95,17 @@ def create_short_url(payload: CreateUrlSchema, db: Session = Depends(get_db)) ->
             short_url=new_url.short_url,
             url=new_url.base_url
         )
+        
+
+@app.get('/{short_slug}')
+def redirect_to_long_url(short_slug: str, db: Session = Depends(get_db)):
+    url_entry = db.query(ValueUrl).filter( ValueUrl.short_url == short_slug).first()
+    
+    if not url_entry:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Short URL not found"
+        )
+    
+    return RedirectResponse(
+        url=url_entry.base_url, status_code=status.HTTP_301_MOVED_PERMANENTLY
+    )
